dd94c51c6626e9dfa726eeb07320642f154034f3
Mukesh <mukeshveerapathiran@gmail.com>
2026-07-20 15:25:58 +0530

    Created using Colab

---
+++ operators.py
@@ -0,0 +1,76 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPg2iHRrb1Jtakmc4/eb+Wr",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/villian-sukuna/python/blob/main/operators.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "a=10\n",
+        "b=20\n",
+        "c=30\n",
+        "print(a+b)\n",
+        "print(a-b)\n",
+        "print(a*b)\n",
+        "print(a/b)\n",
+        "if a==b and a==c:\n",
+        "  print(True)\n",
+        "else:\n",
+        "  print(False)"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "41BjH0428heP",
+        "outputId": "d313b053-fd23-4d02-868e-bd09be36ef93"
+      },
+      "execution_count": 4,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "30\n",
+            "-10\n",
+            "200\n",
+            "0.5\n",
+            "False\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "CkaMAA398iAR"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
